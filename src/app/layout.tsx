aca71fb761cd8ac1734e8efa66f76c929a4b5dc4
nack098 <77537263+nack098@users.noreply.github.com>
2023-07-20 13:47:14 +0700

    fix(Layout-and-css): remove auto generate css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Provider from "@/components/provider";
+import Navigation from "@/components/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full w-full">
-      <body className={inter.className}>{children}</body>
+      <Provider>
+        <body className={`${inter.className} flex h-full w-full`}>
+          <Navigation />
+          {children}
+        </body>
+      </Provider>
     </html>
   );
 }
